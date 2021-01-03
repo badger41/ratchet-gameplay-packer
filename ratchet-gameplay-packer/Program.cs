66d7879c7cadad8c3ea81d8a82d9884adf60f8ad
Connor <Connor@CONNOR-PC>
2021-01-03 12:27:55 -0700

    Added visual studio project

---
+++ ratchet-gameplay-packer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ratchet_gameplay_packer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
